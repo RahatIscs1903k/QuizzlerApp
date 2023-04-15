7750dbbfba42ec86fd9f086989f858625dd68c09
yessenali_54782 <Rakhat.Yessenali@Kaspi.kz>
2023-04-16 01:35:52 +0600

    design: buttoms,labels,texts

--- QuizzlerApp/ViewController.swift
+++ QuizzlerApp/ViewController.swift
@@ -2,18 +2,42 @@
 //  ViewController.swift
 //  QuizzlerApp
 //
-//  Created by Kaspi on 15.04.2023.
+//  Created by Rakhat on 15.04.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButtom: UIButton!
+    @IBOutlet weak var falseButtom: UIButton!
+    
+    let quiz = [
+    "Four + Two is equal to Six",
+    "Five - Three is greater than One",
+    "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        updateUI()
+        
     }
 
-
+    @IBAction func answerButtomPresser(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber ]
+    }
+    
 }
 
